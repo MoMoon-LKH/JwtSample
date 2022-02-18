e928726bc090c15c0f3de9bac093c3cf7b447a1c
MoMoon-LKH <rlgh28@naver.com>
2022-02-18 13:39:18 +0900

    프로젝트 생성 및 application 설정

---
+++ jwtSample/src/test/java/com/group/jwtSample/JwtSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.jwtSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
